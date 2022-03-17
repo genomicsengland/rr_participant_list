7af37fb0cb9adcf5c0a3bf79eda5318af47fa272
Simon Thompson <simonthompson99@googlemail.com>
2022-03-17 16:52:47 +0000

    fix(manual amendments) remove manual amendments to eligibility
    PMI has been updated to account for multiple medical review entries per
    participant so no longer necessary to manually include participants

--- subsequent_withdrawals.sql
+++ subsequent_withdrawals.sql
@@ -30,40 +30,7 @@ _100k_ineligible as (
     where dg_c.concept_code = '100k_ineligibility' and
         dg.blacklisted = false and
         dg.stale = false and 
-        it_c.concept_code = '100k_participant_id' and 
-        /* need to manually not exclude some participants as PMI wrongly failing
-        them */
-        sp.identifier_value not in (
-            '117000309',
-            '122006762',
-            '111004512',
-            '115017910',
-            '112000056',
-            '118003463',
-            '115013311',
-            '115003305',
-            '120001116',
-            '112003271',
-            '111004516',
-            '122000007',
-            '210019145',
-            '111004511',
-            '111000013',
-            '111004515',
-            '114001604',
-            '117000291',
-            '111004517',
-            '120000582',
-            '115016335',
-            '120000581',
-            '115017391',
-            '120000512',
-            '120000946',
-            '117002256',
-            '111000000',
-            '118003461',
-            '112000878'
-        )
+        it_c.concept_code = '100k_participant_id'
 )
 select sp.identifier_value
 from pmi.study_participant sp
@@ -72,6 +39,6 @@ join (select uid from _100k_full_withdrawals union select uid from _100k_ineligi
 join (select participant_id from rr_participant_list.participant_list where eligible_for_release = true) pl 
     on pl.participant_id = sp.identifier_value
 join pmi.concept it_c 
-    on c.uid = sp.identifier_type_cid 
+    on it_c.uid = sp.identifier_type_cid 
 where it_c.concept_code = '100k_participant_id'
 ;
